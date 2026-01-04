a40004a8fd108ed4ccbe6716c7caaacd2399d9dc
Jetaofgak <75139221+Jetaofgak@users.noreply.github.com>
2026-01-04 04:25:25 +0100

    Delete TP18 Client directory

--- TP18 Client/src/main/java/ma/project/tp18client/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.project.tp18client.repositories;
-
-import ma.project.tp18client.entities.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
